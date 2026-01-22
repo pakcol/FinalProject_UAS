ee0ea99b72e37a787f3dbdcf1516b6f131ae468c
Stivent Nathaniel Riwu Laky <110383079+pakcol@users.noreply.github.com>
2026-01-22 17:42:05 +0700

    Update User model with tribe relationship
    Add tribe relationship and update fillable fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,11 +10,17 @@ class User extends Authenticatable
     use Notifiable;
 
     protected $fillable = [
-        'name', 'email', 'password', 'username', 'is_admin'
+        'name', 
+        'email', 
+        'password', 
+        'username', 
+        'tribe_id',
+        'is_admin'
     ];
 
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 
+        'remember_token',
     ];
 
     protected $casts = [
@@ -22,16 +28,33 @@ class User extends Authenticatable
         'is_admin' => 'boolean',
     ];
 
+    /**
+     * Get the tribe selected by user
+     */
+    public function tribe()
+    {
+        return $this->belongsTo(Tribe::class);
+    }
+
+    /**
+     * Get the kingdom owned by user
+     */
     public function kingdom()
     {
         return $this->hasOne(Kingdom::class);
     }
 
+    /**
+     * Get battles as attacker
+     */
     public function battlesAsAttacker()
     {
         return $this->hasManyThrough(Battle::class, Kingdom::class, 'user_id', 'attacker_id');
     }
 
+    /**
+     * Get battles as defender
+     */
     public function battlesAsDefender()
     {
         return $this->hasManyThrough(Battle::class, Kingdom::class, 'user_id', 'defender_id');
